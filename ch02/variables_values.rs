a1b94f8bab684f35c55e35b12356f0882693772e
timothy adigun <adigun.timothy@gmail.com>
2022-10-01 16:38:59 +0100

    modified rust file

--- ch02/variables_values.rs
+++ ch02/variables_values.rs
@@ -7,4 +7,23 @@ fn main() {
     let mut number = 12;
 
     println!("{}", number);
+
+    let number2;
+    let number3;
+
+    number2 = 34;
+    number3 = number2;
+
+    println!("{} {}", number2, number3);
+
+    // Boolean variables and values
+    let mut truth = true;
+    let mut failsy = false;
+
+    println!("{}, {}, {}", truth, failsy, -50 < 6);
+
+    truth = 5 < 2;
+    failsy = 3 != 5;
+
+    println!("{} {}", truth, failsy);
 }
