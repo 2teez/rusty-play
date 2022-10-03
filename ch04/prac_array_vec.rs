e19fddc9031c256f69c7e7615c3656f775a01d1f
timothy adigun <adigun.timothy@gmail.com>
2022-10-03 11:34:19 +0100

    modified rust file

--- ch04/prac_array_vec.rs
+++ ch04/prac_array_vec.rs
@@ -12,4 +12,27 @@ fn main() {
     //x = ["A", "B"];
     //x = [1, 45, 8];
     println!("{:?}", x);
+
+    let mut names: [&str; 4] = [""; 4];
+    names[0] = "timothy";
+    names[1] = "tolulope";
+    println!("{:?}", names);
+    names[2] = "melchizedek";
+    names[3] = "temiloluwa";
+    println!("{:?}", names);
+
+    let family: [[&str; 2]; 2] = [["tim", "tolu"], ["melchi", "temi"]];
+    for index in 0..family.len() {
+        println!("{} {:?}", index, family[index]);
+    }
+
+    for grp in family {
+        let mut names = grp;
+        for name in names.iter_mut() {
+            // work on String later....
+            //*name = &(name.to_owned() + &" adigun".to_owned());
+            //name = format!("{} {}", name, "adigun");
+            print!("{}, ", name);
+        }
+    }
 }
