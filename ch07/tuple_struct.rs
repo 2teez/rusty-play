0f749eb098e939a9906b6b2c67da4fc13d9b37a4
timothy adigun <adigun.timothy@gmail.com>
2022-10-04 00:14:45 +0100

    modified gitignore file

---
+++ ch07/tuple_struct.rs
@@ -0,0 +1,15 @@
+// a tuple like struct
+// can be used like namedtuple in python
+#[derive(Debug)]
+struct Persons<'a>(&'a str, u8, char);
+
+fn main() {
+    let tomi = ("tomi", 15, "female");
+    let kunle: (&str, u8, &str) = ("kunle", 18, "male");
+
+    println!("{:#?}, {:#?}", tomi, kunle);
+    println!("Age: {}", kunle.1);
+
+    let tomi = Persons("tomi", 34, 'F');
+    println!("{:#?}", tomi);
+}
