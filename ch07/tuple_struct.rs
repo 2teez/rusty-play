ee5291301b8a6d90ba46474ed8989c682564fb8f
timothy adigun <adigun.timothy@gmail.com>
2022-10-04 00:29:41 +0100

    added tuple_struct file

--- ch07/tuple_struct.rs
+++ ch07/tuple_struct.rs
@@ -1,7 +1,15 @@
+#[allow(dead_code)]
 // a struct like tuple
 // can be used like namedtuple in python
 #[derive(Debug)]
 struct Persons<'a>(&'a str, u8, char);
+
+#[derive(Debug)]
+struct Person {
+    name: &'static str,
+    age: u8,
+    gender: char,
+}
 
 fn main() {
     let tomi = ("tomi", 15, "female");
@@ -13,4 +21,13 @@ fn main() {
     let tomi = Persons("tomi", 34, 'F');
     println!("{:#?}", tomi);
     println!("Name: {}, Gender: {}", tomi.0, tomi.2);
+
+    let mut tim = Person {
+        name: "timothy",
+        age: 23,
+        gender: 'M',
+    };
+    println!("{:#?}", tim);
+    tim.age = 40;
+    println!("{:#?}", tim);
 }
