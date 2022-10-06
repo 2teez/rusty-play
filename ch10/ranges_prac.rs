8d8c590447ce9798a721f5902dfdea699a05a934
timothy adigun <adigun.timothy@gmail.com>
2022-10-06 08:51:13 +0100

    modified range_prac file

--- ch10/ranges_prac.rs
+++ ch10/ranges_prac.rs
@@ -7,4 +7,25 @@ fn main() {
     for letter in alphabet {
         println!("{}", letter);
     }
+
+    // take a slice of the alphabet
+    let alphabet = ('a'..'z').collect::<Vec<_>>();
+    for letter in &alphabet[10..] {
+        print!("{} ", letter);
+    }
+
+    println!();
+
+    for letter in &alphabet[..10] {
+        print!("{} ", letter);
+    }
+    println!();
+
+    println!("{:?}", &alphabet[..]);
+    println!("{:?}", alphabet);
+
+    let s = "abc012è€";
+    for n in 0..s.len() {
+        println!("{}: {:?}", n, s.as_bytes()[n]);
+    }
 }
