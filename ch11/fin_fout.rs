af61e59a856bd1145e1b9411355a4e6b6a5787ef
timothy adigun <adigun.timothy@gmail.com>
2022-10-08 21:28:21 +0100

    modified rust file

--- ch11/fin_fout.rs
+++ ch11/fin_fout.rs
@@ -1,7 +1,7 @@
 #![allow(dead_code, unused)]
 
 use std::env::{args, vars};
-use std::io::{stdin, stdout, Write};
+use std::io::{self, stdin, stdout, BufRead, Read, Write};
 
 fn main() {
     println!("[{:?}] [{:?}]", args(), vars());
@@ -19,10 +19,51 @@ fn main() {
     }
 
     // read a line from stdin
-    stdout().write("Enter your name: ".as_bytes()).unwrap();
+    //stdout().write("Enter your name: ".as_bytes()).unwrap();
     let mut line = String::new();
-    let input = stdin().read_line(&mut line);
+    //let input = stdin().read_line(&mut line);
+    let input = get_user_input("Enter your name: ");
     if input.is_ok() {
-        println!("{}", line);
+        println!("{}", input.unwrap());
     }
+
+    // print out an input
+    print!("{}", "Enter> ");
+    io::stdout().flush();
+    stdin().read_line(&mut line).unwrap();
+    println!("{}", line);
+
+    // read out a whole file
+    print!("{}", "Enter a filename: ");
+    io::stdout().flush();
+
+    let mut filename = String::new();
+    io::stdin().read_line(&mut filename).unwrap();
+    filename = filename.trim().to_string();
+    let file = std::fs::File::open(filename).unwrap();
+    let mut buf = io::BufReader::new(file);
+    for line in buf.lines() {
+        println!(
+            "{}",
+            match line {
+                Ok(line) => line,
+                Err(_) => "error...".to_string(),
+            }
+        );
+    }
+
+    let name = get_user_input("Enter a number: ");
+    if name.is_ok() {
+        println!("{}", name.unwrap());
+    }
+}
+
+fn get_user_input<'a>(msg: &'a str) -> Result<String, std::io::Error> {
+    print!("{}", msg);
+    io::stdout().flush();
+
+    // line to use
+    let mut line = String::new();
+    io::stdin().read_line(&mut line)?;
+    Ok(line)
 }
