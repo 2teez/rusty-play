8fd29c4b6c5a97d7f0a3400bb667aae4e20aaebe
timothy adigun <adigun.timothy@gmail.com>
2023-02-12 22:44:21 +0100

    modified rs file

--- ch03/types.rs
+++ ch03/types.rs
@@ -3,6 +3,10 @@
 fn main() {
     println!("{:?}", build_vector());
     println!("{:?}", '8'.to_digit(10));
+
+    let words = "A town hall different from Blalablueu...";
+    let (first, second) = words.split_at(21);
+    println!("{} <=> {}", first, second);
 }
 
 fn build_vector() -> Vec<i16> {
