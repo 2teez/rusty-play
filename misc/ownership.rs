5c138355fc1dda608bc18b27a539d2fc1f82f140
timothy adigun <adigun.timothy@gmail.com>
2023-02-14 22:33:20 +0100

    modified rs file

--- misc/ownership.rs
+++ misc/ownership.rs
@@ -6,6 +6,12 @@ struct Person <'a> {
     birth: usize,
 }
 
+#[derive(Debug, Clone)]
+struct PPerson {
+    name: String,
+    birth: i32,
+}
+
 fn main() {
     let mut ppl = Vec::new();
     ppl.push(Person {name: "timothy", birth: 1987});
@@ -15,8 +21,16 @@ fn main() {
     for person in &ppl {
         println!("{}", print_person(person));
     }
+
+    let kunle = PPerson {name: "kunle".to_string(), birth: 1980};
+    print!("{}", print_pperson(kunle.clone()));
+    println!("{:?}", kunle);
 }
 
 fn print_person(person: &Person) -> String {
+    format!("In {}, {} was born.", person.birth, person.name)
+}
+
+fn print_pperson(person: PPerson) -> String {
     format!("In {}, {} was born.", person.birth, person.name)
 }
